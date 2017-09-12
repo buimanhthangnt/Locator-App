c6926b5483131ebf3416fd172c76c4a52ff236db
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-09-12 23:20:14 +0700

    test angular, ok men

---
+++ app_client/main.js
@@ -0,0 +1,4 @@
+angular.module('locator_app')
+.controller('mainCtrl', function($scope) {
+	$scope.author = "Bui Manh Thang";
+});
---
+++ readme.txt
@@ -0,0 +1,2 @@
+Locator Application
+Where to find places to study, work and dating with your friends.
