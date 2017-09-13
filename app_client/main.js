5ec53d5037a33f36b74053f1b5d03c424db12578
Dang The <thedangnguyen2204@gmail.com>
2017-09-13 14:49:02 +0700

    Fix study route

--- app_client/main.js
+++ app_client/main.js
@@ -1,19 +1,19 @@
 angular.module('locator_app', ['ngRoute']);
 
-function config($routeProvider) {
+function config($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'home/home.view.html',
 			controller: 'homeCtrl',
 		})
 		.when('/study', {
-			templateUrl: 'option/study.view.html',
+			templateUrl: 'option/study/study.view.html',
 			controller: 'studyCtrl',
 		})
 		.otherwise({redirectTo: '/'});
-
+		$locationProvider.html5Mode(true);
 }
 
 angular
 	.module('locator_app')
-	.config(['$routeProvider', config]);
+	.config(['$routeProvider', '$locationProvider', config]);
