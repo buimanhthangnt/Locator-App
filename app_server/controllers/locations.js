48d929166fb537df5159d9551f4d1d4e70c84530
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-10-10 15:23:58 +0700

    done api location study

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,28 +1,53 @@
 let db = require('../db');
 
-let sendJsonResponse = function(res, status, content) {
+let sendJsonResponse = function (res, status, content) {
 	res.status(status);
 	res.json(content);
 };
 
-module.exports.locationsStudy = function(req, res) {
-	db.connect().then((connection) => {
-		connection.query('SELECT * FROM users', (err, results, fields) => {
-			if (err) sendJsonResponse(res, 200, {status: err});
-			else sendJsonResponse(res, 200, results);
-		});
-	})
-}
-
-module.exports.locationList = function (req, res) {
+module.exports.locationStudy = function (req, res) {
 	let sql = "SELECT * FROM locations where type = 'study'";
-	// let sql = "select distinct * from locations inner join keywords on keywords.location_id = locations.id where type = 'study'";
 	db.connect().then((connection) => {
 		connection.query(sql, (err, results, fields) => {
-			if (err) throw err;
-			results = JSON.stringify(results);
-			// console.log(results);
-			sendJsonResponse(res, 200, results);
+			if (err) sendJsonResponse(res, 400, { err: true, msg: err });
+			else {
+				let locations = results;
+				let sql2 = "SELECT * FROM keywords";
+				connection.query(sql2, (err2, results2, fields2) => {
+					if (err2) sendJsonResponse(res, 400, { err: true, msg: err2 });
+					else {
+						let currentIndex = 0;
+						locations.forEach((location) => {
+							location.keywords = [];
+							for (let i = currentIndex; i < results2.length; i++) {
+								if (results2[i].location_id == location.id) location.keywords.push(results2[i].keyword);
+								else if (results2[i].location_id > location.id) {
+									currentIndex = i;
+									break;
+								}
+							}
+						});
+						let sql3 = "SELECT * FROM opening_times";
+						connection.query(sql3, (err3, results3, field3) => {
+							if (err3) sendJsonResponse(res, 400, { err: true, msg: err3 });
+							else {
+								let currentIndex = 0;
+								locations.forEach((location) => {
+									location.openingTimes = [];
+									for (let i = currentIndex; i < results3.length; i++) {
+										if (results3[i].location_id == location.id) location.openingTimes.push(results3[i]);
+										else if (results3[i].location_id > location.id) {
+											currentIndex = i;
+											break;
+										}
+									}
+								});
+								sendJsonResponse(res, 200, locations);
+							}
+						});
+					}
+				});
+			}
 		});
 	});
 }
@@ -36,13 +61,13 @@ module.exports.addLocation = function (req, res) {
 	}
 	db.connect().then((connection) => {
 		connection.query(sql, [[values]], (err, results, fields) => {
-			if (err) sendJsonResponse(res, 400, {err: true, msg: err});
+			if (err) sendJsonResponse(res, 400, { err: true, msg: err });
 			else {
 				let location_id = results.insertId;
 				let sql2 = "INSERT INTO keywords (keyword, location_id) VALUES ?";
 				let values2 = [];
 				if (!req.body.keywords || !Array.isArray(req.body.keywords)) {
-					sendJsonResponse(res, 400, {err: true, msg: 'keywords is not array'});
+					sendJsonResponse(res, 400, { err: true, msg: 'keywords is not array' });
 					return;
 				}
 				req.body.keywords.forEach((keyword) => {
@@ -50,12 +75,12 @@ module.exports.addLocation = function (req, res) {
 					values2.push(value2);
 				});
 				connection.query(sql2, [values2], (err2, results2, fields2) => {
-					if (err2) sendJsonResponse(res, 400, {err: true, msg: err2});
+					if (err2) sendJsonResponse(res, 400, { err: true, msg: err2 });
 					else {
 						let sql3 = "INSERT INTO opening_times (day, state, open, close, location_id) VALUES ?";
 						let values3 = [];
 						if (!req.body.openingTimes || !Array.isArray(req.body.openingTimes)) {
-							sendJsonResponse(res, 400, {err: true, msg: 'openingTimes is not array'});
+							sendJsonResponse(res, 400, { err: true, msg: 'openingTimes is not array' });
 							return;
 						}
 						req.body.openingTimes.forEach((openingTime) => {
@@ -63,8 +88,8 @@ module.exports.addLocation = function (req, res) {
 							values3.push(value3);
 						});
 						connection.query(sql3, [values3], (err3, results3, fields3) => {
-							if (err3) sendJsonResponse(res, 400, {err: true, msg: err3});
-							else sendJsonResponse(res, 200, {err: false, msg: 'Location has just added'});
+							if (err3) sendJsonResponse(res, 400, { err: true, msg: err3 });
+							else sendJsonResponse(res, 200, { err: false, msg: 'Location has just added' });
 						});
 					}
 				});
