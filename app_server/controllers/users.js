ec90782e0d8e8d4c764a092c0f31441f4370347d
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-10-19 13:30:07 +0700

    authorize middleware

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -2,25 +2,24 @@ let db = require('../db');
 let services = require('../services');
 
 module.exports.usersReadOne = (req, res) => {
-    let payload = services.verifyJwt(req.get('jwt'));
-    if (!payload) {
-        services.responseUnauthorized(res);
-        return;
+    let userJwt = req.get('jwt');
+    let next = (payload) => {
+        db.connect()
+          .then(() => {
+              let sql = `SELECT id, name, email FROM users WHERE email = "${payload.email}"`;
+              return db.select(sql);
+          })
+          .then(users => {
+              if (Array.isArray(users) && users.length == 1) {
+                  services.sendJsonResponse(res, 200, {err: false, msg: 'Success', data: users[0]});
+              } else {
+                  throw new Error('Bad request');
+              }
+          })
+          .catch(err => {
+              console.error(err);
+              services.sendJsonResponse(res, 400, {err: true, msg: '' + err});
+          })
     }
-    db.connect()
-      .then(() => {
-          let sql = `SELECT id, name, email FROM users WHERE email = "${payload.email}"`;
-          return db.select(sql);
-      })
-      .then(users => {
-          if (Array.isArray(users) && users.length == 1) {
-              services.sendJsonResponse(res, 200, {err: false, msg: 'Success', data: users[0]});
-          } else {
-              throw new Error('Bad request');
-          }
-      })
-      .catch(err => {
-          console.error(err);
-          services.sendJsonResponse(res, 400, {err: true, msg: '' + err});
-      })
+    services.authorize(res, userJwt, next);
 }
--- app_server/services.js
+++ app_server/services.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').load();
 
 //need to check expired date
-module.exports.verifyJwt = (reqToken) => {
+let verifyJwt = (reqToken) => {
     let payload;
     jwt.verify(reqToken, process.env.JWT_SECRET, (err, decoded) => {
         if (!err) payload = decoded;
@@ -10,10 +10,17 @@ module.exports.verifyJwt = (reqToken) => {
     return payload;
 }
 
-module.exports.responseUnauthorized = (res) => {
-    res.status(401);
-    res.json({err: true, msg: 'Unauthorized'});
+module.exports.authorize = (res, jwt, next) => {
+    let payload = verifyJwt(jwt);
+    if (!payload) {
+        res.status(401);
+        res.json({err: true, msg: 'Unauthorized'});
+    } else {
+        next(payload);
+    }
 }
+
+module.exports.verifyJwt;
 
 module.exports.sendJsonResponse = (res, status, content) => {
   res.status(status);
