3b2270ba2cd0fef34c61e4a71e01d254fd7fc818
Dang The <thedangnguyen2204@gmail.com>
2017-10-24 14:23:59 +0700

    addReview api

--- app_server/controllers/reviews.js
+++ app_server/controllers/reviews.js
@@ -1,10 +1,37 @@
 let services = require('../services');
 let db = require('../db');
+const jwt = require('jsonwebtoken');
+require('dotenv').load();
 
 module.exports.addReview = function (req, res) {
 	let location_id = req.params.locationid;
+	let userJwt = req.get('jwt');
+	let getUserId = (userJwt) => {
+		let payload;
+		jwt.verify(userJwt, process.env.JWT_SECRET, (err, decoded) => {
+			if (!err) payload = decoded;
+		});
+		db.connect()
+		.then(() => {
+				let sql = `SELECT id, name, email FROM users WHERE email = "${payload.email}"`;
+				return db.select(sql)[0].data.id;
+		})
+		.then(users => {
+			if (Array.isArray(users) && users.length == 1) {
+					services.sendSuccessResponse(res, users[0]);
+			} else {
+					throw new Error('Bad request');
+			}
+		})
+		.catch(err => {
+				console.error(err);
+				services.sendFailResponse(res, err);
+		})
+	};
+	let user_id = getUserId(userJwt);
 	db.connect()
 	.then(() => {
+		let created_time = Date.now();
 		let sql = "INSERT INTO comments (rating, content, created_time, location_id, user_id) VALUES ?"
 		let values = [];
 		for (let prop in req.body) {
