8304bf1192b52f006e23e33e86e729bad7414c40
Dang The <thedangnguyen2204@gmail.com>
2017-10-25 10:07:02 +0700

    update userReadOne API, get all comments of a user

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -2,6 +2,7 @@ let db = require('../db');
 let services = require('../services');
 
 module.exports.usersReadOne = (req, res) => {
+    let user;
     let userJwt = req.get('jwt');
     services.authorize(userJwt)
         .then(payload => {
@@ -11,11 +12,23 @@ module.exports.usersReadOne = (req, res) => {
                   return db.select(sql);
               })
               .then(users => {
-                  if (Array.isArray(users) && users.length == 1) {
-                      services.sendSuccessResponse(res, users[0]);
-                  } else {
-                      throw new Error('Bad request');
-                  }
+                  if (!Array.isArray(users) && users.length != 1) throw new Error("No user found!");
+                  user = users[0];
+                  let id = user.id;
+                  let sql = `SELECT * FROM comments WHERE user_id="${id}"`;
+                  return db.select(sql);
+              })
+              .then(_comments => {
+                let comments = [];
+                _comments.forEach((_comment) => {
+                    comments.push(_comment);
+                })
+                user.comments = comments;
+                services.sendSuccessResponse(res, user);
+              })
+              .catch(err => {
+                console.error(err);
+                services.sendFailResponse(res, err);
               });
         })
         .catch(err => {
