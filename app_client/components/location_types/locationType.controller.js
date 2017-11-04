9e3bb5be838fca1fd1321301bf890fe70131f990
Dang The <thedangnguyen2204@gmail.com>
2017-11-04 22:24:23 +0700

    Add sort button

--- app_client/components/location_types/locationType.controller.js
+++ app_client/components/location_types/locationType.controller.js
@@ -1,12 +1,13 @@
 (function() {
     angular.module('locator_app').controller('locationTypeCtrl', locationTypeCtrl);
-    locationTypeCtrl.$inject = ['$scope', '$http', '$routeParams', '$timeout', 'geolocation'];
-    function locationTypeCtrl($scope, $http, $routeParams, $timeout, geolocation) {
+    locationTypeCtrl.$inject = ['$scope', '$http', '$routeParams', '$timeout', 'geolocation', '$route'];
+    function locationTypeCtrl($scope, $http, $routeParams, $timeout, geolocation, $route) {
         $scope.pageSize = 6;
         $scope.type = $routeParams.type;
         var locationList = function() {
             $http.get(`/api/locations/type/${$routeParams.type}`).then(function success(response) {
                 $scope.locations = response.data.data;
+                $scope.numberOfLocations = $scope.locations.length;
 								let coords = [];
 								$scope.locations.forEach(location => {
 									coords.push({long: location.longitude, lat: location.latitude});
@@ -17,7 +18,7 @@
 										newLocations[i].distance = distances[i];
 									}
 									$timeout (() => {
-									  $scope.locations = newLocations;
+                                      $scope.locations = newLocations;
 									}, 0);
 								}, err => {
 									throw new Error(err);
@@ -25,6 +26,28 @@
             }, function error(err) {
                 console.log(err);
             });
+        };
+
+        $scope.sortByRating = function() {
+            function compare(a, b) {
+                return b.rating - a.rating;
+            }
+            $scope.locations = $scope.locations.sort(compare);
+        };
+
+        $scope.sortByDistance = function() {
+            function compare(a, b) {
+                if (a.distance.charAt(a.distance.length - 2) == 'k' && b.distance.charAt(b.distance.length - 2) == 'k')
+                    return Number(a.distance.slice(0, a.distance.length - 2)) - Number(b.distance.slice(0, b.distance.length - 2));
+                else if (a.distance.charAt(a.distance.length - 2) != 'k' && b.distance.charAt(b.distance.length - 2) == 'k')
+                    return 1;
+                else if (a.distance.charAt(a.distance.length - 2) == 'k' && b.distance.charAt(b.distance.length - 2) != 'k')
+                    return 1;
+                else if (a.distance.charAt(a.distance.length - 2) != 'k' && b.distance.charAt(b.distance.length - 2) != 'k')
+                    return Number(a.distance.slice(0, a.distance.length - 1)) - Number(b.distance.slice(0, b.distance.length - 1));
+            }
+            
+            $scope.locations = $scope.locations.sort(compare);
         };
 
         locationList();
