137de40e6963866a317a39276713b9dc8e317f36
Dang The <thedangnguyen2204@gmail.com>
2017-11-07 14:40:25 +0700

    Fix sort by distance

--- app_client/components/location_types/locationType.controller.js
+++ app_client/components/location_types/locationType.controller.js
@@ -7,7 +7,7 @@
         var locationList = function() {
             $http.get(`/api/locations/type/${$routeParams.type}`).then(function success(response) {
                 $scope.locations = response.data.data;
-                $scope.numberOfLocations = $scope.locations.length;
+                $scope.parseFloatOfLocations = $scope.locations.length;
 								let coords = [];
 								$scope.locations.forEach(location => {
 									coords.push({long: location.longitude, lat: location.latitude});
@@ -38,23 +38,18 @@
         $scope.sortByDistance = function() {
             function compare(a, b) {
                 if (a.distance.charAt(a.distance.length - 2) == 'k' && b.distance.charAt(b.distance.length - 2) == 'k')
-                    return Number(a.distance.slice(0, a.distance.length - 2)) - Number(b.distance.slice(0, b.distance.length - 2));
+                    return parseFloat(a.distance.slice(0, a.distance.length - 2)) - parseFloat(b.distance.slice(0, b.distance.length - 2));
                 else if (a.distance.charAt(a.distance.length - 2) != 'k' && b.distance.charAt(b.distance.length - 2) == 'k')
-                    return 1;
+                    return -1;
                 else if (a.distance.charAt(a.distance.length - 2) == 'k' && b.distance.charAt(b.distance.length - 2) != 'k')
                     return 1;
                 else if (a.distance.charAt(a.distance.length - 2) != 'k' && b.distance.charAt(b.distance.length - 2) != 'k')
-                    return Number(a.distance.slice(0, a.distance.length - 1)) - Number(b.distance.slice(0, b.distance.length - 1));
+                    return parseFloat(a.distance.slice(0, a.distance.length - 1)) - parseFloat(b.distance.slice(0, b.distance.length - 1));
             }
             
             $scope.locations = $scope.locations.sort(compare);
         };
 
         locationList();
-        let lat = 21.00229030419171;
-        let long = 105.7931041742529;
-        // geolocation.getDistance(long, lat).then(distance => {
-        //     console.log(distance);
-        // });
     }
 })();
