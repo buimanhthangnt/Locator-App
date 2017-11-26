8608605de83d84ec3ad82b085cbbb7b6bdb2e33a
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-11-26 11:35:07 +0700

    bug. done!!!

--- app_client/components/home.controller.js
+++ app_client/components/home.controller.js
@@ -11,18 +11,23 @@
     vm.currentUser = authentication.currentUser();
     vm.isAdmin = vm.currentUser.email == "admin@gmail.com";
 		vm.searchLocations = [];
+		let timeout;
 		vm.query = function () {
 			if (!vm.searchText || vm.searchText == "") {
 				vm.searchLocations = [];
 				return;
 			}
-			$http.get(`/api/locations/name?name=${vm.searchText}`)
-				.then(res => {
-					vm.searchLocations = res.data.data;
-				}, err => {
-					vm.searchLocations = [];
-					console.log(err);
-				});
+			if (timeout) clearTimeout(timeout);
+			timeout = setTimeout(() => {
+				$http.get(`/api/locations/name?name=${vm.searchText}`)
+					.then(res => {
+						if (vm.searchText == "") vm.searchLocations = [];
+						else vm.searchLocations = res.data.data;
+					}, err => {
+						vm.searchLocations = [];
+						console.log(err);
+					});
+			}, 800);
 		}
 	}
 })();
